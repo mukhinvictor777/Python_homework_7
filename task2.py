02c86f42f8fb783dd90d12cf8f2acdd8c4692987
mukhinvictor777 <mukhinvictor@gmail.com>
2023-01-02 17:16:20 +0500

    task2 done

--- task2.py
+++ task2.py
@@ -7,3 +7,30 @@
 Например: 20м*5000м*25кг*5см = 12500 т
 """
 
+
+class Road:
+
+    thickness = 5
+    weight = 25
+
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def asphalt_weight(self, length, width):
+        if not length:
+            length = self.__length
+        if not width:
+            width = self.__width
+        asphalt_weight = int(length) * int(width) * self.weight * self.thickness
+        print(f'\nМасса асфальта для покрытия дорожного полотна длиной {length} метров и шириной {width} метров ' 
+              f'равна {asphalt_weight//1000} т')
+
+
+example_road = Road('5000', '20')
+example_road.asphalt_weight('', '')
+example_road.asphalt_weight(10000, 50)
+
+
+
+
